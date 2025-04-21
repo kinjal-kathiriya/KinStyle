bbe247ad3e5cfdacc7c655295339fd1cab8a4d59
Kinjal Kathiriya <kinjukathiriya22@gmail.com>
2025-04-21 17:29:29 -0500

    Add files via upload

---
+++ KinStyleTests/KinStyleTests.swift
@@ -0,0 +1,17 @@
+//
+//  KinStyleTests.swift
+//  KinStyleTests
+//
+//  Created by kinjal kathiriya  on 3/9/25.
+//
+
+import Testing
+@testable import KinStyle
+
+struct KinStyleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
